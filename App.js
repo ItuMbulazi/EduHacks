988103a9c2e3f01a0ff7096615f47318ee29c470
gitstep1 <leeannbianca315@gmail.com>
2022-10-06 16:13:05 +0200

    login/signup

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Onboarding from './components/Onboarding';
+import Login from './components/login';
+import SignUp from './components/signup';
+import Packages from './components/packages';
 
 export default function App() {
   return (
     <View style={styles.container}>
      
       <Onboarding/>
+      <Login/>
+      <SignUp/><br></br><br></br><br></br>
+      <Packages/>
+
     </View>
   );
 }
